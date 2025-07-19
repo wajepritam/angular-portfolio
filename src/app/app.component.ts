cc83fc2f49da6dc144dcd0dfa99f557965d14ea6
Pritam Waje <wajepritam@gmail.com>
2025-07-19 12:32:37 +0530

    header and footer changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-portfolio';
+   ngOnInit() {
+  }
 }
